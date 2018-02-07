183b0fd045ac71cfec935204c8586e1f10597caa
Neil García <neilgarcia@Neils-MacBook-Pro.local>
2018-02-07 13:13:09 -0600

    Updating to nodejs

--- public/index.js
+++ public/index.js
@@ -1,12 +1,13 @@
 
-import * as functions from 'firebase-functions'
-import * as google from 'googleapis'
-import * as _ from 'lodash'
+const functions = require('firebase-functions');
+const google = require('googleapis');
+const _ = require('lodash');
 const sheets = google.sheets('v4')
+
 //https://docs.google.com/spreadsheets/d/1Dq9KKRAcadmSDTwWDPICkkDbMOYrZqNvCrVx7fD1pDI/edit#gid=0
 const spreadsheetId = '1Dq9KKRAcadmSDTwWDPICkkDbMOYrZqNvCrVx7fD1pDI'
 
-const serviceAccount = require('../client_secret_800877605555-6qld7s455v4n6od5hm9ot0luavaatqn8.apps.googleusercontent.com.json')
+const serviceAccount = require('./client_secret_800877605555-6qld7s455v4n6od5hm9ot0luavaatqn8.apps.googleusercontent.com.json')
 const jwtClient = new google.auth.JWT(
     serviceAccount.client_email,
     null,
@@ -24,16 +25,16 @@ const jwtAuthPromise = new Promise((resolve, reject) => {
           return
         }
         resolve(tokens)
-    })
-})
+    });
+});
 
-export const copyScoresToSheet = functions.database.ref('/scores').onUpdate(event => {
-    const data = event.data.val()
-    console.info(data)
+exports.copyScoresToSheet = functions.database.ref('/scores').onUpdate(event => {
+    const data = event.data.val();
+    console.info(data);
 
     // Sort the scores.  scores is an array of arrays each containing name and score.
-    const scores = _.map<any, [string, number]>(data, (value, key) => [String(key), value])
-    scores.sort((a,b) => {return b[1] - a[1]})
+    const scores = _.map(data, (value, key) => [String(key), value]);
+    scores.sort((a,b) => {return b[1] - a[1]});
 
     return jwtAuthPromise.then(tokens => {
         return new Promise((resolve, reject) => {
@@ -45,13 +46,13 @@ export const copyScoresToSheet = functions.database.ref('/scores').onUpdate(even
                 resource: { values: scores }
             }, (err, result) => {
                 if (err) {
-                    console.log(err)
-                    reject(err)
+                    console.log(err);
+                    reject(err);
                     return
                 }
-                console.log(result)
-                resolve(result)
-            })
-        })
-    })
-})
+                console.log(result);
+                resolve(result);
+            });
+        });
+    });
+});
